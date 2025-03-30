b8f6ef2e52bc12cab69a920d3a4d670d4ffc0870
laurasarrazola <lausa8a@gmail.com>
2025-03-30 11:54:15 -0500

    agregué un archivo readme en la clase de git

---
+++ Tarea/script/script.js
@@ -0,0 +1,15 @@
+function abrirModal(id) {
+  document.getElementById(id).style.display = 'block';
+  /*.style Accede a los estilos CSS en línea del elemento.*/
+  /* .display Modifica la propiedad display del elemento convirtiéndolo a bloque*/
+}
+
+function cerrarModal(id) {
+  document.getElementById(id).style.display = 'none';
+  /*El valor none oculta el modal*/
+}
+
+
+
+
+
